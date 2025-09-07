c9bae3bca45dbb9e1ce625be98787a25f2df2c63
lucas gonçalves <lucasazeredogoncalves32@gmail.com>
2025-09-07 16:31:54 +0000

    imagem do flash: https://4kwallpapers.com/images/wallpapers/the-flash-gr

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '4kwallpapers.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ import { Label } from "@/components/ui/label"
 
 const backgroundImages = [
   { src: "https://poltronanerd.com.br/wp-content/uploads/2020/06/b28117ea-41bc-47fe-93ef-f82a1f31717b.jpg", alt: "Série Dexter", hint: "Dexter series" },
-  { src: "https://picsum.photos/1920/1080?random=22", alt: "Série Flash", hint: "Flash series" },
+  { src: "https://4kwallpapers.com/images/wallpapers/the-flash-grant-gustin-barry-allen-tv-series-dc-comics-3840x2160-6761.jpg", alt: "Série Flash", hint: "Flash series" },
   { src: "https://picsum.photos/1920/1080?random=23", alt: "Série One Piece", hint: "One Piece anime" },
   { src: "https://picsum.photos/1920/1080?random=24", alt: "Série La Casa de Papel", hint: "La Casa de Papel" },
   { src: "https://picsum.photos/1920/1080?random=25", alt: "Série Breaking Bad", hint: "Breaking Bad" },
@@ -34,7 +34,7 @@ const backgroundImages = [
 
 export default function LoginPage() {
   const plugin = React.useRef(
-    Autoplay({ delay: 3000, stopOnInteraction: true, stopOnMouseEnter: true })
+    Autoplay({ delay: 3000, stopOnInteraction: false, stopOnMouseEnter: true })
   )
    const router = useRouter()
 
@@ -52,7 +52,7 @@ export default function LoginPage() {
           loop: true,
         }}
         onMouseEnter={plugin.current.stop}
-        onMouseLeave={plugin.current.play}
+        onMouseLeave={plugin.current.reset}
       >
         <CarouselContent>
           {backgroundImages.map((img, index) => (
@@ -64,6 +64,7 @@ export default function LoginPage() {
                   fill
                   className="object-cover"
                   data-ai-hint={img.hint}
+                  priority={index === 0}
                 />
               </div>
             </CarouselItem>
