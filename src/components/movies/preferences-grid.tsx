d91b7d8eb3d0f9c57111b1e19310d3e753454757
lucas gonçalves <lucasazeredogoncalves32@gmail.com>
2025-09-17 00:30:57 +0000

    adicione algumas animações de transição de entrada nos banners. quando e

--- src/components/movies/preferences-grid.tsx
+++ src/components/movies/preferences-grid.tsx
@@ -79,12 +79,14 @@ export default function PreferencesGrid() {
   return (
     <>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 md:gap-6">
-        {movies.map((movie) => (
+        {movies.map((movie, index) => (
           <MovieCard
             key={movie.id}
             movie={movie}
             onSelect={handleSelectMovie}
             isSelected={selectedMovieIds.has(movie.id)}
+            className="animate-fade-in-up"
+            style={{ animationDelay: `${index * 50}ms` }}
           />
         ))}
       </div>
