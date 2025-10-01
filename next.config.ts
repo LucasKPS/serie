05a5611b7ac3ad3341e05aba6050109111d36fb2
lucas gonçalves <lucasazeredogoncalves32@gmail.com>
2025-10-01 00:48:06 +0000

    continue alterando os card da proxima tela, ja colocamos a do dexter, vo

--- next.config.ts
+++ next.config.ts
@@ -76,6 +76,12 @@ const nextConfig: NextConfig = {
         hostname: 'm.media-amazon.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn2.gstatic.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
