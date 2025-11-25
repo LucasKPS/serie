5133192b9b9ac06fa713f603bf5f8885641514db
lucas gonçalves <lucasazeredogoncalves32@gmail.com>
2025-11-25 03:48:18 +0000

    a foto do card ainda nao apareceu, o link ta ai: https://br.web.img2.acs

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,7 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'm.media-amazon.com' },
       { protocol: 'https', hostname: 'upload.wikimedia.org' },
       { protocol: 'https', hostname: 'br.web.img3.acsta.net' },
+      { protocol: 'https', hostname: 'br.web.img2.acsta.net' },
       { protocol: 'https', hostname: 'encrypted-tbn0.gstatic.com' },
       { protocol: 'https', hostname: 'encrypted-tbn1.gstatic.com' },
       { protocol: 'https', hostname: 'encrypted-tbn2.gstatic.com' },
@@ -23,13 +24,4 @@ const nextConfig: NextConfig = {
       { protocol: 'https', hostname: 'www.europanet.com.br' },
       { protocol: 'https', hostname: 'picsum.photos' },
       { protocol: 'https', hostname: 'placehold.co' },
-      { protocol: 'https', hostname: 'images.alphacoders.com' },
-      { protocol: 'https', hostname: 'picfiles.alphacoders.com' },
-      { protocol: 'https', hostname: 'images8.alphacoders.com' },
-      { protocol: 'https', hostname: 'images5.alphacoders.com' },
-      { protocol: 'https', hostname: 'images2.alphacoders.com' },
-    ],
-  },
-};
-
-export default nextConfig;
+      { protocol
